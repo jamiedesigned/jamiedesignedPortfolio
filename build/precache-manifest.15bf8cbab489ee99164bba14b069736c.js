7c28bee1916301110429cae29967bfc20f038043
jamiedesigned <hello@jamiedesigned.com>
2019-03-27 22:36:06 +0000

    first upload
    first upload

---
+++ build/precache-manifest.15bf8cbab489ee99164bba14b069736c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "ef80e607ed874939a526db24ed4af358",
+    "url": "/jamiedesignedPortfolio/static/media/withzing.ef80e607.png"
+  },
+  {
+    "revision": "dd51fed13df1930d64c2f9b89f3c2734",
+    "url": "/jamiedesignedPortfolio/static/media/victimadviceline2.dd51fed1.png"
+  },
+  {
+    "revision": "88a9a552ddce1719f885cab2ef7636ae",
+    "url": "/jamiedesignedPortfolio/static/media/sandridge2.88a9a552.png"
+  },
+  {
+    "revision": "8f82959a27f47583eedfa5f1581d3ebe",
+    "url": "/jamiedesignedPortfolio/static/media/jamiedesignedfinal.8f82959a.svg"
+  },
+  {
+    "revision": "12c756d8e8faf9136c918c18cc4779a1",
+    "url": "/jamiedesignedPortfolio/static/media/engtech.12c756d8.png"
+  },
+  {
+    "revision": "9f062f9d74d2e9d19c66",
+    "url": "/jamiedesignedPortfolio/static/js/runtime~main.4a988329.js"
+  },
+  {
+    "revision": "9561d293df6b3870b106",
+    "url": "/jamiedesignedPortfolio/static/js/main.48e8ad37.chunk.js"
+  },
+  {
+    "revision": "a2af4740a4c2a25ef583",
+    "url": "/jamiedesignedPortfolio/static/js/2.871091d0.chunk.js"
+  },
+  {
+    "revision": "9561d293df6b3870b106",
+    "url": "/jamiedesignedPortfolio/static/css/main.edc78935.chunk.css"
+  },
+  {
+    "revision": "efffed1e5327d405115b5437fb850753",
+    "url": "/jamiedesignedPortfolio/index.html"
+  }
+];
